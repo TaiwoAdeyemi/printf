c3bdaa12c06a948fdc09a752dd50bba8dfce8e1e
TaiwoAdeyemi <Tadeyemi004@gmail.com>
2023-07-25 16:01:53 -0700

    printf

---
+++ print_pointer.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdio.h>
+
+int _strcmp(char *, char *);
+
+/**
+ * print_pointer - print a number in hexadecimal format
+ * @list: number to print
+ *
+ * Return:Length of the number
+ */
+
+int print_pointer(va_list list)
+{
+	char *p_buff;
+	int size;
+
+	p_buff = itoa(va_arg(list, unsignedling int), 16);
+
+	if (!_strcmp(p_buff, "0"))
+		return (print("(nil)"));
+
+	size = print("0x");
+
+	if (!_strcmp(p_buff, "-1"))
+		size += print("ffffffffffffffff");
+	else
+		size += print(p_buff);
+
+	return (size);
+}
+
+/**
+ * _strcmp - compare two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: integer
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = o; s1[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	}
+
+	return (0);
+}
