ae3a7608d17ffeee6fb0693778db6c0fe2d15986
TaiwoAdeyemi <Tadeyemi004@gmail.com>
2023-07-25 14:23:56 -0700

    printf

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Printf
+Education is when you read the fine print. Experience is what you get if you don't
--- main.h
+++ main.h
@@ -16,6 +16,7 @@ int percent_handler(const char *, va_list, int *);
 int print_string(va_list);
 int print_char(va_list);
 int print_integer(va_list);
+int print_binary(va_list);
 
 int _putchar(char);
 int buffer(char);
